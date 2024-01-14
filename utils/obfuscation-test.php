6207512ece512d851e8ab6a5e2f7b7e54e17f88d
slackero <slackero@gmail.com>
2024-01-14 22:22:06 +0100

    Use native PHP to read IBAN files
    Based on globalcitizen/php-iban#126

--- utils/obfuscation-test.php
+++ utils/obfuscation-test.php
@@ -1,11 +1,20 @@
 <?php
 require_once dirname(__DIR__) . '/php-iban.php';
 
-$dir = dirname(__FILE__);
-$ibans = `cat $dir/example-ibans/*`;
-$lines = explode("\n", $ibans);
-foreach ($lines as $iban) {
+$example_iban_files = glob(__DIR__ . '/example-ibans/*');
+if (count($example_iban_files) == 0) {
+    echo 'No example IBAN files found in "' . __DIR__ . '/example-ibans"' . "\n";
+    exit(1);
+}
+
+$ibans = [];
+foreach ($example_iban_files as $file) {
+    if (is_readable($file)) {
+        $ibans += file($file);
+    }
+}
+foreach($ibans as $iban) {
     $iban = iban_to_machine_format($iban);
-    print iban_to_human_format($iban) . "\n";
-    print iban_to_obfuscated_format($iban) . "\n";
+    echo iban_to_human_format($iban) . "\n";
+    echo iban_to_obfuscated_format($iban) . "\n";
 }
