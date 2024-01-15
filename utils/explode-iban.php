82d0d809a19905911f39c3f97ab69e90ec399182
Oliver Georgi <oliver.georgi@ionos.com>
2024-01-15 07:38:51 +0100

    Enhanced explode-iban.php by IBAN validation

--- utils/explode-iban.php
+++ utils/explode-iban.php
@@ -1,12 +1,14 @@
 <?php
 require_once dirname(__DIR__) . '/php-iban.php';
 
-if (!isset($argv[1])) {
+if (empty($argv[1])) {
     print 'Usage: ' . $argv[0] . " <IBAN>\n";
     print "  where <IBAN> is a valid IBAN to display the components of.\n";
     exit(1);
 }
 $iban = $argv[1];
-$parts = iban_get_parts($argv[1]);
+echo 'Is ' . $iban . ' a valid IBAN: ' . (verify_iban($iban) ? 'Yes' : 'No');
+echo PHP_EOL;
+$parts = iban_get_parts($iban);
 print_r($parts);
 exit(0);
